59e26e5fe40c7d6f5576cd92be045ccbeed1873d
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-03-25 17:30:20 +0300

    Update DeveloperProjectFinder.java

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -6,5 +6,14 @@ import java.util.Set;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+      List<String> qwe = new ArrayList<>();
+
+        for (Map.Entry<String, Set<String>> entry : projects.entrySet()) {
+            if (String.valueOf(entry.getValue()).contains(developer)){
+                    qwe.add(entry.getKey());
+            }
+        }
+
+        return qwe;
     }
 }
