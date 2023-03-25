85ecf7070c9cb6e3586eeafe6eb4c438014474fb
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-03-25 17:29:57 +0300

    Update LessonsGetter.java

--- src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
+++ src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
@@ -6,5 +6,23 @@ import java.util.Set;
 
 public class LessonsGetter {
     public Set<String> getLessons(Map<String, List<String>> timetable) {
+        Set<String> qwe = new HashSet<>();
+
+        for (Map.Entry<String, List<String>> entry : timetable.entrySet()) {
+            qwe.add(String.valueOf(timetable.values()));
+        }
+
+        String s = "";
+        for (String ss : qwe) {
+            s = s + ss;
+        }
+        s = s.replace("[", "");
+        s = s.replace("]", "");
+        s = s.replace(" ", "");
+        String[] q = s.split(",");
+
+        Set<String> qw = new HashSet<>(Arrays.asList(q));
+
+        return qw;
     }
 }
