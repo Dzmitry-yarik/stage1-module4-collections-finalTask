c9d13ca96f47e2d869711e538c154c91c0fd26e9
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-03-25 17:29:25 +0300

    Update MapFromKeysCreator.java

--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -5,5 +5,25 @@ import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> qwe = new HashMap<>();
+
+        List<String> str = new ArrayList<>();
+        Set<String> set = new HashSet<>();
+
+        for (Map.Entry<String, Integer> entry : sourceMap.entrySet()) {
+            str.add(entry.getKey());
+        }
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < str.size(); j++) {
+                if (i == str.get(j).length()) {
+                    set.add(str.get(j));
+                    qwe.put(i, set);
+                }
+            }
+            set = new HashSet<>();
+        }
+
+        return qwe;
     }
 }
